4d2d5c8eb72036509606ce3dea8437232f052315
Sebastian Procopos <sprocopo@c2r5s6.wethinkcode.co.za>
2020-07-09 14:29:28 +0200

    day02 ex00 prints out the alphabet

---
+++ Day02/ex00/ft_print_alphabet.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sprocopo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/09 13:47:27 by sprocopo          #+#    #+#             */
+/*   Updated: 2020/07/09 14:22:14 by sprocopo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_print_alphabet(void)
+
+{
+	write (1, "abcdefghijklmnopqrstuvwxyz", 26);
+}
+
+int	main(void)
+
+{
+	ft_print_alphabet();
+	return 0;
+}
