df819e70b5fccedbc9ad1a0b098efca5743de064
Sebastian Procopos <sprocopo@c2r5s6.wethinkcode.co.za>
2020-07-10 09:31:59 +0200

    Day02 ex01 Function only

--- Day02/ex01/ft_print_reverse_alphabet.c
+++ Day02/ex01/ft_print_reverse_alphabet.c
@@ -6,7 +6,7 @@
 /*   By: sprocopo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 14:43:20 by sprocopo          #+#    #+#             */
-/*   Updated: 2020/07/09 14:45:18 by sprocopo         ###   ########.fr       */
+/*   Updated: 2020/07/10 09:30:59 by sprocopo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,4 @@ void ft_print_reverse_alphabet(void)
 
 {
 	write (1, "zyxwvutsrqponmljkihgfedcba", 26);
-}
-
-int	main(void)
-
-{
-	ft_print_reverse_alphabet();
-	return 0;
 }
