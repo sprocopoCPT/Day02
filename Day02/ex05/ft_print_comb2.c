9d685045492ee5dbed1dc39c0b594672e71a7b42
sprocopoCPT <67952406+sprocopoCPT@users.noreply.github.com>
2020-07-11 16:19:13 +0200

    Add files via upload

--- Day02/ex05/ft_print_comb2.c
+++ Day02/ex05/ft_print_comb2.c
@@ -6,27 +6,49 @@
 /*   By: sprocopo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/10 10:18:46 by sprocopo          #+#    #+#             */
-/*   Updated: 2020/07/10 10:50:15 by sprocopo         ###   ########.fr       */
+/*   Updated: 2020/07/11 16:02:31 by sprocopo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+void ft_putchar(char c);
 
-void ft_putchar (char c)
+void ft_print_comb2(void)
 {
-	write (1,&c,1);
+	char a = '0';
+	char b = '0';
+	char c = '0';
+	char d = '0';
+
+	while (a <= '9')
+	{
+		while (b <= '9')
+		{	
+			while (c <= '9')
+			{
+				while (d <= '9')
+				{
+					if ((a < c) || (a == c && b < d))
+					{	
+						ft_putchar(a);
+						ft_putchar(b);
+						ft_putchar(' ');
+						ft_putchar(c);
+						ft_putchar(d);
+						if (a != '9' || b != '9' || c != '9' || d != '9')
+						{
+							ft_putchar(',');
+							ft_putchar(' ');
+						}
+					}
+					d++;
+				}
+				d = '0';
+				c++;
+			}
+			c = '0';
+			b++;
+		}
+    b = '0';
+    a++;
+	}
 }
-
-void ft_putnbr(int nbr)
-{
-
-	ft_putchar('&nbr');
-}
-
-int main (void)
-{
-	int nbr;
-	nbr = 6;
-		ft_putnbr(nbr);
-}
-
