05f3506edc84b3efe7cafa8f805029dcac565801
Sebastian Procopos <sprocopo@c2r5s6.wethinkcode.co.za>
2020-07-11 15:37:20 +0200

    commit

--- Day02/ex00/ft_print_alphabet.c
+++ Day02/ex00/ft_print_alphabet.c
@@ -6,7 +6,7 @@
 /*   By: sprocopo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 13:47:27 by sprocopo          #+#    #+#             */
-/*   Updated: 2020/07/10 09:27:24 by sprocopo         ###   ########.fr       */
+/*   Updated: 2020/07/11 15:12:53 by sprocopo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,10 @@ void ft_print_alphabet(void)
 
 {
 	write (1, "abcdefghijklmnopqrstuvwxyz", 26);
+}
+
+int main(void)
+{
+	ft_print_alphabet();
+	return 0;
 }
